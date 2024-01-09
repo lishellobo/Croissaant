0fbfc9af313e2cd8a3e79532dd9ce57e1bdd35f1
PlatypusPus <145650588+PlatypusPus@users.noreply.github.com>
2024-01-09 23:05:49 +0530

    fixed the routing

--- app.py
+++ app.py
@@ -21,46 +21,12 @@ def home():
 
 @app.route("/login.html",methods=['GET','POST'])
 def login():
-    mesage = ''
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        email = request.form['email']
-        password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password, ))
-        user = cursor.fetchone()
-        if user:
-            session['loggedin'] = True
-            session['userid'] = user['userid']
-            session['name'] = user['name']
-            session['email'] = user['email']
-            mesage = 'Logged in successfully !'
-            return render_template('index.html', mesage = mesage)
-        else:
-            mesage = 'Please enter correct email / password !'
-    return render_template('login.html', mesage = mesage)
+    return render_template("login.html")
 
+@app.route("/register.html",methods=["GET","POST"])
 def register():
-    mesage = ''
-    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
-        userName = request.form['name']
-        password = request.form['password']
-        email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
-        account = cursor.fetchone()
-        if account:
-            mesage = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            mesage = 'Invalid email address !'
-        elif not userName or not password or not email:
-            mesage = 'Please fill out the form !'
-        else:
-            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password, ))
-            mysql.connection.commit()
-            mesage = 'You have successfully registered !'
-    elif request.method == 'POST':
-        mesage = 'Please fill out the form !'
-    return render_template('login.html', mesage = mesage)
+    return render_template("register.html")
+
 
 if __name__=='__main__':
     app.run(debug=True)
