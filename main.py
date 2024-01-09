659e6edbf95ec27675fc98df457bf23c722e19bc
PlatypusPus <145650588+PlatypusPus@users.noreply.github.com>
2024-01-09 12:35:13 +0530

    Connected to home in login flas

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask,redirect,url_for,render_template,request
 
-app=Flask(_name_)
+app=Flask(__name__)
 
-@app.route("/",methods=['POST','GET'])
+@app.route("/",)
 def home():
     return render_template("index.html")
 
-@app.route("/login.html",methods=['POST','GET'])
+@app.route("/login.html")
 def login():
     return render_template("login.html")
 
-if _name=='__main_':
+if __name__=='__main__':
     app.run(debug=True)
